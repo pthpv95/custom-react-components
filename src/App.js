e30ae3492fbb0c210f4b3ee3a5e1c431aeb81e57
Hien Pham <thehienpv@gmail.com>
2021-01-25 22:38:43 +0700

    add env

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NumberFormat from './Customs/CustomNumberFormat';
 function App() {
   return (
     <div className="App">
+      <h1>Hello {process.env.REACT_APP_NOT_SECRET_CODE}</h1>
       <NumberFormat value={'123x456'} onValueChanged={(value) => {
         console.log(value);
       }} />
